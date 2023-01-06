3125faa2841364a5a77c938cc267e3f4ebc650e7
JinWoo_ <829kong@naver.com>
2023-01-06 09:51:13 +0900

    2023-01-06 업뎃

---
+++ Source/BaseProject/InitGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InitGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASEPROJECT_API AInitGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
